5b80ecc1374fcb0508fa24c7c2e523ecbbfe1b90
Mohamad Alabudi <80290328+malabudi@users.noreply.github.com>
2021-11-27 19:03:55 -0500

    Updated the main program and unit test
    - The main program has been updated and its file name is now ticket_viewer.py
    
    - Another file has been added that uses the unittest module to run unit tests for the ticket viewer program

---
+++ test_ticket_viewer.py
@@ -0,0 +1,95 @@
+import unittest
+from unittest.mock import patch
+import ticket_viewer
+
+# auth: ("mohamadalabudi42@gmail.com/token", "bvJwx9uKm6ieRuYv0P0jmA7YojKhwfBUYmbSV1jx")
+
+class Test_Ticket_Viewer(unittest.TestCase):
+
+    # Define/Setup variables useful for unit testing
+    def setUp(self):
+        print("\nSet up\n")
+
+        # Set up the auth tuple for unit tests
+        self.auth = ("mohamadalabudi42@gmail.com/token", "bvJwx9uKm6ieRuYv0P0jmA7YojKhwfBUYmbSV1jx")
+
+
+
+    # Tear the variable(s) in set upafter unit testing
+    def tearDown(self):
+        print("\nTear down\n")
+
+    
+
+    # Test the view_ticket_by_id function
+    def test_view_ticket_by_id(self):
+        with patch('ticket_viewer.requests.get') as mocked_get:
+
+            # -----Test Valid ticket ID URL-----
+            mocked_get.return_value.ok = True
+
+            # The return value or response of the view_ticket_by_id function
+            ticket = ticket_viewer.view_ticket_by_id('2112', self.auth)
+
+            # make sure the get request was called with the correct url
+            mocked_get.assert_called_with("https://zccmalabudi5807.zendesk.com/api/v2/requests/2112", auth = self.auth)
+
+            # Display the string that the function returns
+            print(ticket)
+
+            # Check if the output is what we expect with the unit test
+            self.assertEqual(ticket, "Success")
+
+
+            # -----Test Invalid ticket ID URL (numerical ID, ID 1 is invalid because it has been deleted and can no longer be used)------
+            mocked_get.return_value.ok = False
+
+            ticket = ticket_viewer.view_ticket_by_id('1', self.auth)
+
+            # make sure the get request was called with the bad url
+            mocked_get.assert_called_with("https://zccmalabudi5807.zendesk.com/api/v2/requests/1", auth = self.auth)
+
+            print(ticket)
+
+            self.assertEqual(ticket, "Bad response")
+
+
+            # -----Test Invalid ticket ID URL (non-numerical user input)-----
+            mocked_get.return_value.ok = False
+
+            ticket = ticket_viewer.view_ticket_by_id('abc', self.auth)
+
+            # make sure the get request was called with the bad url
+            mocked_get.assert_called_with("https://zccmalabudi5807.zendesk.com/api/v2/requests/abc", auth = self.auth)
+
+            print(ticket)
+
+            self.assertEqual(ticket, "Bad response")
+
+    
+
+    # Test the view_tickets function
+    def test_view_tickets(self):
+
+        # patch used as a context manager that mocks a get request
+        with patch('ticket_viewer.requests.get') as mocked_get:
+
+            #-----Test Valid URL-----
+            mocked_get.return_value.ok = True
+
+            # The return value or response of the view_tickets function
+            ticket = ticket_viewer.view_tickets(self.auth)
+
+            # make sure the get request was called with the correct url
+            mocked_get.assert_called_with("https://zccmalabudi5807.zendesk.com/api/v2/requests/", auth = self.auth)
+
+            # Print the string that the test functions returns
+            print(ticket)
+
+            # Check if the output is what we expect with the unit test
+            self.assertEqual(ticket, "Success")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
